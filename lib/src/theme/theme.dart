d8fd07feca3e0d3e2ed2163b4fed20662f2578ab
Aash7787 <ashir11boss@gmail.com>
2025-03-12 14:14:56 +0500

    “theme_dark_and_light_added”

--- lib/src/theme/theme.dart
+++ lib/src/theme/theme.dart
@@ -26,13 +26,27 @@ class AppTheme {
     );
   }
 
-  static final ColorScheme colorSchemeLight = ColorScheme.fromSeed(
-    seedColor: Colors.blue,
+  static final ColorScheme colorSchemeLight = ColorScheme(
+    primary: Colors.blue,
+    onPrimary: Colors.white,
+    secondary: Colors.orange,
+    onSecondary: Colors.white,
+    surface: Colors.grey[100]!,
+    onSurface: Colors.black,
+    error: Colors.red,
+    onError: Colors.white,
     brightness: Brightness.light,
   );
 
-  static final ColorScheme colorSchemeDark = ColorScheme.fromSeed(
-    seedColor: Colors.blue,
+  static final ColorScheme colorSchemeDark = ColorScheme(
+    primary: Colors.blue[700]!,
+    onPrimary: Colors.black,
+    secondary: Colors.orange[700]!,
+    onSecondary: Colors.black,
+    surface: Colors.grey[900]!,
+    onSurface: Colors.white,
+    error: Colors.red[400]!,
+    onError: Colors.black,
     brightness: Brightness.dark,
   );
 
