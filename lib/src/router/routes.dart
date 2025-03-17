fb3c5a80cf4e7cfc641fab67a13d08c2937ba8b0
Aash7787 <ashir11boss@gmail.com>
2025-03-17 12:47:22 +0500

    “auth_remove”

--- lib/src/router/routes.dart
+++ lib/src/router/routes.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_vpn/src/features/auth/login/login_screen.dart';
-import 'package:flutter_vpn/src/features/auth/signup/pages/forgot_password_screen.dart';
-import 'package:flutter_vpn/src/features/auth/signup/pages/sign_up_screen.dart';
 import 'package:flutter_vpn/src/features/home/page/about_page.dart';
 import 'package:flutter_vpn/src/features/home/page/country_pick_page.dart';
 import 'package:flutter_vpn/src/features/home/page/home_page.dart';
@@ -16,11 +13,6 @@ Route<dynamic> onGenRoute(RouteSettings settings) {
     HomePage.route => buildPageWithDefaultTransition(child: HomePage()),
     AboutPage.route => buildPageWithDefaultTransition(child: AboutPage()),
     SettingPage.route => buildPageWithDefaultTransition(child: SettingPage()),
-    SignUpScreen.route => buildPageWithDefaultTransition(child: SignUpScreen()),
-    LoginScreen.route => buildPageWithDefaultTransition(child: LoginScreen()),
-    ForgotPasswordScreen.route => buildPageWithDefaultTransition(
-      child: ForgotPasswordScreen(),
-    ),
     CountryPickPage.route => buildPageWithDefaultTransition(
       child: CountryPickPage(),
     ),
